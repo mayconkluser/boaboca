e46d7d524c0159305470402690421b6b50306ab4
Maycon Looma_Code <mayconkluser@loomacode.com.br>
2022-05-15 18:12:28 -0300

    add btn-mobile

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const links = document.querySelectorAll(".menu a");
+// LINHA ABAIXO DO MENU
+const links = document.querySelectorAll("#menu a");
 
 console.log();
 
@@ -14,3 +15,21 @@ function ativarLink(link) {
   console.log(href);
 }
 links.forEach(ativarLink);
+
+// BTN-MOBILE
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
